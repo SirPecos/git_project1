d501c8b9273b2c62f704592bc9dbde45dc53969c
SirPecos <151543784+SirPecos@users.noreply.github.com>
2023-11-27 18:11:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import random
+
+
+class Yellow_Ellipses(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw(self, qp):
+        r = random.randint(1, 100)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(random.randint(0, 740), random.randint(0, 450), r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Yellow_Ellipses()
+    ex.show()
+    sys.exit(app.exec())
